59432dda32d096bb60c786d658e0a63c22c616d6
Kemuel Kesley <kemuelkesleytkd@outlook.com>
2023-09-20 11:59:35 -0300

    pequenas alterações

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from usuarios.views import login
 
+#Rota para a página de login
 urlpatterns = [
-    path('login', login, name='login'),    
+    path('', login, name='login'),    
 ]
