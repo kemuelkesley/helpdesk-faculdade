a7e0bc36a22cef4019aa753123affba5cd53b8c9
Kemuel Kesley <kemuelkesleytkd@outlook.com>
2023-09-24 20:47:10 -0300

    criado a opção para baixar um relatorio no formato CSV.

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from cadastro.views import equipamentos
+from cadastro.views import equipamentos, exportar_csv
 
 
 urlpatterns = [
-    path("equipamentos", equipamentos, name="equipamentos")
+    path("equipamentos", equipamentos, name="equipamentos"),
+    path("exportar_csv", exportar_csv, name="exportar_csv"),
 ]
--- cadastro/views.py
+++ cadastro/views.py
@@ -1,3 +1,5 @@
+import csv
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Equipamento
 
@@ -11,3 +13,27 @@ def equipamentos(request):
     return render(request, "dados/equipamentos.html", {"equipamentos" : equipamentos})
 
 
+
+def exportar_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="equipamentos.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow([
+        'Nome',
+        'N Serie',
+        'Descricao',
+        'Condominio'
+    ])
+
+    equipamentos = Equipamento.objects.all()
+
+    for equipamento in equipamentos:
+        writer.writerow([
+            equipamento.nome,
+            equipamento.numero_serie,
+            equipamento.descricao,
+            equipamento.condominio
+        ])
+
+    return response
