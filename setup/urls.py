279c878c4a12c1f80cff3acc34639b48be1198d1
Kemuel Kesley <79339726+kemuelkesley@users.noreply.github.com>
2025-04-15 23:26:50 -0300

    criado uma forma para redirecionar o site para a pagina de login

--- setup/urls.py
+++ setup/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+
+# Redireciona a raiz "/" para "/login/"
+path('', lambda request: redirect('login')),
 
 
 urlpatterns = [
