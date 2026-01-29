3e015d09b17d05b6eb0322f0b1c4604bc5ddd047
kemuelkesley <kemuelkesleytkd@outlook.com>
2026-01-29 14:19:30 -0300

    alterada o allowedhosts  no settings do django para rodar em produção no render.com em ambiente de desenvolvimento ou seja teste.

--- setup/settings.py
+++ setup/settings.py
@@ -33,7 +33,7 @@ DEBUG = False
 
 # Lembrar de o host correto 'helpdesk-libk.onrender.com'
 # Se For testar local tirar o 'helpdesk-libk.onrender.com' e deixar apenas vazio []
-ALLOWED_HOSTS = ['helpdesk-libk.onrender.com']
+ALLOWED_HOSTS = ['https://helpdesk-faculdade-test.onrender.com']
 
 
 # Application definition
